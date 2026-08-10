a66f6b4a61a58b5077021a337e5eadba369db3ec
Ashutosh-Maheshwari1 <ashutoshmaheshwari275@gmail.com>
2026-08-11 01:49:32 +0530

    fix: update Flutter to 3.44.1 and set minSdk=23 for Firebase compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.meditrack.meditrack_ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion // Firebase requires minSdk 23+
+        minSdk = 23 // Firebase requires minSdk 23+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
